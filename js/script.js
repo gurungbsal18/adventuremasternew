b713da82e8bce3aaf3375bfe6c6a3e637b4c6ca0
gurungbsal18 <gurungbsal18@gmail.com>
2023-07-21 00:52:08 +0545

    trip page and script file added

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// let scrollToTop = document.querySelector(".scroll-top");
+let mobilePrice = document.querySelector(".mobile-trip-price");
+
+window.onscroll = function () {
+  scrollMobilePrice();
+};
+
+function scrollMobilePrice() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    mobilePrice.style.display = "block";
+    // scrollToTop.sylte.display = "block";
+  } else {
+    mobilePrice.style.display = "none";
+    // scrollToTop.sylte.display = "none";
+  }
+}
+
+//   let scrollToTop = document.querySelector(".scroll-top");
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollMobilePrice() {
+//   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+//     scrollToTop.style.display = "block";
+//   } else {
+//     scrollToTop.style.display = "none";
+//   }
+// }
+
+// scrollToTop.addEventListener("click", function () {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// });
