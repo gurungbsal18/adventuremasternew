3396e2807c30a60c48081bc13c4864845261f254
Bishal Gurung <60152891+gurungbsal18@users.noreply.github.com>
2023-07-21 10:35:20 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,18 +15,18 @@ function scrollMobilePrice() {
   }
 }
 
-  let scrollToTop = document.querySelector(".scroll-top");
-window.onscroll = function () {
-  scrollFunction();
-};
+//   let scrollToTop = document.querySelector(".scroll-top");
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    scrollToTop.style.display = "block";
-  } else {
-    scrollToTop.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+//     scrollToTop.style.display = "block";
+//   } else {
+//     scrollToTop.style.display = "none";
+//   }
+// }
 
 scrollToTop.addEventListener("click", function () {
   document.body.scrollTop = 0;
