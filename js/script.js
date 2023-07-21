a913c6f9a3525e0001874af60855c690c69013c1
gurungbsal18 <gurungbsal18@gmail.com>
2023-07-22 01:09:49 +0545

    script updated

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-// let scrollToTop = document.querySelector(".scroll-top");
-
 window.onscroll = function () {
   let mobilePrice = document.querySelector(".mobile-trip-price");
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     mobilePrice.style.display = "block";
-    // scrollToTop.sylte.display = "block";
   } else {
     mobilePrice.style.display = "none";
-    // scrollToTop.sylte.display = "none";
   }
 };
-
-// let scrollToTop = document.querySelector(".scroll-top");
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-//     scrollToTop.style.display = "block";
-//   } else {
-//     scrollToTop.style.display = "none";
-//   }
-// }
 
 window.addEventListener("scroll", function () {
   let scrollToTop = document.querySelector(".scroll-top");
