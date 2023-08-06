dec2ba918f8955ca24e2a5b97d856fe43fa9f452
Bishal Gurung <gurungbsal18@gmail.com>
2023-08-06 21:37:24 +0545

    menu updated

--- js/menu.js
+++ js/menu.js
@@ -20,14 +20,34 @@ mobileMenuBar.addEventListener("click", function () {
 
 let menuItem1 = document.getElementById("menu-item-1");
 let menuItem2 = document.getElementById("menu-item-2");
+let menuItem3 = document.getElementById("menu-item-3");
+let menuItem4 = document.getElementById("menu-item-4");
 let menuList1 = document.getElementById("menu-list-1");
 let menuList2 = document.getElementById("menu-list-2");
+let menuList3 = document.getElementById("menu-list-3");
+let menuList4 = document.getElementById("menu-list-4");
 
 menuItem1.addEventListener("click", function () {
   menuList1.style.display = "block";
   menuList2.style.display = "none";
+  menuList3.style.display = "none";
+  menuList4.style.display = "none";
 });
 menuItem2.addEventListener("click", function () {
-  menuList1.style.display = "none";
   menuList2.style.display = "block";
+  menuList1.style.display = "none";
+  menuList3.style.display = "none";
+  menuList4.style.display = "none";
+});
+menuItem3.addEventListener("click", function () {
+  menuList1.style.display = "none";
+  menuList2.style.display = "none";
+  menuList3.style.display = "block";
+  menuList4.style.display = "none";
+});
+menuItem4.addEventListener("click", function () {
+  menuList1.style.display = "none";
+  menuList2.style.display = "none";
+  menuList3.style.display = "none";
+  menuList4.style.display = "block";
 });
