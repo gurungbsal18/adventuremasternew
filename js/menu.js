a8b45b8aae4e3e7c6e073d529b747bbc745f3863
gurungbsal18 <gurungbsal18@gmail.com>
2023-08-06 19:02:10 +0545

    responsive css added, expland menu updated on progress

--- js/menu.js
+++ js/menu.js
@@ -17,3 +17,17 @@ mobileMenuBar.addEventListener("click", function () {
   searchBar.style.display = "block";
   //   console.log("helo");
 });
+
+let menuItem1 = document.querySelector(".menu-item-1");
+let menuItem2 = document.querySelector(".menu-item-2");
+let menuList1 = document.querySelector(".menu-list-1");
+let menuList2 = document.querySelector(".menu-list-2");
+
+menuItem1.addEventListener("click", function () {
+  menuList1.style.display = "block";
+  menuList2.style.display = "hide";
+});
+menuItem2.addEventListener("click", function () {
+  menuList1.style.display = "hide";
+  menuList2.style.display = "block";
+});
