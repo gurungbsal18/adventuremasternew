1e6ea2b9826940fd3ae2c36fd9f2871d06047755
Bishal Gurung <gurungbsal18@gmail.com>
2023-08-13 02:45:36 +0545

    read more or less fixed of explore region page

--- js/script.js
+++ js/script.js
@@ -10,54 +10,82 @@ window.onscroll = function () {
 
 // // scroll to the top
 let scrollToTop = document.querySelector(".scroll-top");
-window.addEventListener("scroll", function () {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    scrollToTop.style.display = "block";
-  } else {
-    scrollToTop.style.display = "none";
-  }
-});
+document.addEventListener('DOMContentLoaded', function (){
+  window.addEventListener("scroll", function () {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      scrollToTop.style.display = "block";
+    } else {
+      scrollToTop.style.display = "none";
+    }
+  });
+  
+  scrollToTop.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+  
+})
 
-scrollToTop.addEventListener("click", function () {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
+// explore region content read more and less
+let exploreRegionContent = document.querySelector('.explore-region-content');
+let readMoreBtn = document.getElementById('content-read-more');
+
+document.addEventListener('DOMContentLoaded', function (){
+  readMoreBtn.addEventListener('click', function () {
+    if(exploreRegionContent.classList.contains('collapsed')) {
+      exploreRegionContent.classList.remove('collapsed');
+      exploreRegionContent.classList.add('expand')
+      readMoreBtn.innerText = 'Read Less'
+    } else {
+      exploreRegionContent.classList.remove('expand')
+      exploreRegionContent.classList.add('collapsed')
+      readMoreBtn.innerText = "Read More"
+    }
+  })
+  
+})
+
 
 // // trip navbar sticky
 let navBar = document.querySelector(".trip-nav");
 const initialOffset = navBar.offsetTop;
-window.addEventListener("scroll", function () {
-  // Get the current scroll position
-  const scrollPos = window.scrollY;
-
-  if (scrollPos >= initialOffset) {
-    navBar.style.position = "fixed";
-    navBar.classList.add("sticky-trip-nav");
-    // navBar.style.width = "100%";
-    // navBar.style.top = "0";
-  } else {
-    navBar.style.position = "relative";
-    navBar.classList.remove("sticky-trip-nav");
-  }
-});
+document.addEventListener('DOMContentLoaded', function (){
+  window.addEventListener("scroll", function () {
+    // Get the current scroll position
+    const scrollPos = window.scrollY;
+  
+    if (scrollPos >= initialOffset) {
+      navBar.style.position = "fixed";
+      navBar.classList.add("sticky-trip-nav");
+      // navBar.style.width = "100%";
+      // navBar.style.top = "0";
+    } else {
+      navBar.style.position = "relative";
+      navBar.classList.remove("sticky-trip-nav");
+    }
+  });
+})
 
 // cost include exclude and trip highlight tab section
 let costIncludeTab = document.querySelector(".cost-include-tab");
 let costExcludeTab = document.querySelector(".cost-exclude-tab");
 
-costIncludeTab.addEventListener("click", function () {
-  document.querySelector(".cost-include-tab-content").style.display = "block";
-  document.querySelector(".cost-exclude-tab-content").style.display = "none";
-  costIncludeTab.classList.add('btn-success');
-  costIncludeTab.classList.remove('btn-outline-success')
-  costExcludeTab.classList.add('btn-outline-danger')
-  costExcludeTab.classList.remove('btn-danger')
-});
-costExcludeTab.addEventListener("click", function () {
-  document.querySelector(".cost-exclude-tab-content").style.display = "block";
-  document.querySelector(".cost-include-tab-content").style.display = "none";
-  costExcludeTab.classList.add('btn-danger')
-  costExcludeTab.classList.remove('btn-outline-danger')
-  costIncludeTab.classList.add('btn-outline-success')
-  costIncludeTab.classList.remove('btn-success');
-});
+document.addEventListener('DOMContentLoaded', function (){
+  costIncludeTab.addEventListener("click", function () {
+    document.querySelector(".cost-include-tab-content").style.display = "block";
+    document.querySelector(".cost-exclude-tab-content").style.display = "none";
+    costIncludeTab.classList.add('btn-success');
+    costIncludeTab.classList.remove('btn-outline-success')
+    costExcludeTab.classList.add('btn-outline-danger')
+    costExcludeTab.classList.remove('btn-danger')
+  });
+  costExcludeTab.addEventListener("click", function () {
+    document.querySelector(".cost-exclude-tab-content").style.display = "block";
+    document.querySelector(".cost-include-tab-content").style.display = "none";
+    costExcludeTab.classList.add('btn-danger')
+    costExcludeTab.classList.remove('btn-outline-danger')
+    costIncludeTab.classList.add('btn-outline-success')
+    costIncludeTab.classList.remove('btn-success');
+  });
+  
+})
